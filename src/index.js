b254dbba58285996fd43e992d4b3e3b55cec841c
urvashi <urvashikshatriya9@gmail.com>
2023-08-11 00:02:49 +0530

    remove router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
     <App />
-  </Router>
 );
 reportWebVitals();
